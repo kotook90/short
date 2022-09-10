5554c543efc41e48b52478401a6fb141296f9f82
kotook90 <99076387+kotook90@users.noreply.github.com>
2022-09-10 16:28:06 +0300

    Update logrus.go

--- logrus/logrus.go
+++ logrus/logrus.go
@@ -2,24 +2,26 @@ package logrus
 
 import (
 	logger "github.com/sirupsen/logrus"
-	"log"
-
 	"os"
+	"sync"
 )
 
-func LogsInit() *logger.Entry {
 
+func logInit() (*os.File, *logger.Entry) {
 	logger.SetFormatter(&logger.JSONFormatter{})
 	standardFields := logger.Fields{}
+	logFile, err := os.OpenFile("error.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		logger.Fatalf("Error opening file: %v", err)
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	logger.SetOutput(logFile)
+	wg.Done()
+	wg.Wait()
 	hlog := logger.WithFields(standardFields)
-	f, err := os.OpenFile("logs/error.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("Error opening file: %v", err)
-	}
-	err = f.Close()
-	if err != nil {
-		logger.Errorf("Файл логов не закрылся %s", err)
-	}
-	log.SetOutput(f)
-	return hlog
+
+	return logFile, hlog
 }
